cd4210653d3c88895c87d544c01f118505d3c16e
Shekh Rasel Masrur Ahmmad Nissan <shekhraselmasrurahmmadnissan@gmail.com>
2023-04-09 11:35:12 +0600

    Type Conversion.

---
+++ typeConversion.js
@@ -0,0 +1,20 @@
+// const kg = 'name';
+
+// const pricePerKG = 65;
+// const ppkString = pricePerKG.toString();
+
+// const intKG = parseInt(kg);
+
+// const total = intKG * pricePerKG;
+// const stringTotal = kg * ppkString;
+// console.log(ppkString);
+// console.log(stringTotal);
+
+const pi = '3.1416';
+const convertPI = parseFloat(pi);
+// console.log(convertPI);
+
+// 2 decimal point...
+const twoDecimal = convertPI.toFixed(2);
+// const sixDecimal = convertPI.toFixed(6);
+console.log(parseFloat(twoDecimal));
